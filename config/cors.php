7b74c218cdb990518c0cd76c5d6cd3ed0a71e6e1
navysigma <niorugarox@gmail.com>
2026-06-12 16:44:16 +0700

    feat: cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'https://threadly.arcv.web.id'), 
+        'http://localhost:5173'
+    ],
+
 
     'allowed_origins_patterns' => [],
 
