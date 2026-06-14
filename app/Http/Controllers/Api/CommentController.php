1ac74a9eb0ca22df3e97d1531da9d03ce13189d2
navysigma <niorugarox@gmail.com>
2026-06-14 13:11:41 +0700

    bug EDIT COMMENT

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
+use App\Models\CommentEditHistory;
 use App\Models\Like;
 use App\Models\Post;
 use App\Models\User;
